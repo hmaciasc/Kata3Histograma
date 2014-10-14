9e0c97459f1c39171203d0a45887a0b942d8d7a3
usuario <usuario@10.22.143.12>
2014-10-14 14:18:59 +0100

    MailReader.readDomains defined and implemented. Main init

---
+++ src/histograma/Kata3Histogram.java
@@ -0,0 +1,11 @@
+package histograma;
+
+public class Kata3Histogram {
+    
+    public static void main(String[] args){
+        MailReader reader = new MailReader("mails.txt");
+        HistogramBuilder<String> builder = new HistogramBuilder<>();
+        Histogram<String> histogram = builder.build(reader.readDomains());
+        new HistogramViewer<String>().print(histogram);
+    }
+}
