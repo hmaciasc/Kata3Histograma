abcadb034016db540f6496d26a6a22afd1b937fe
usuario <usuario@10.22.143.12>
2014-10-14 13:26:03 +0100

    HashMap.get redefined to return 0 if it doesn't contains a key

--- src/histograma/HistogramBuilder.java
+++ src/histograma/HistogramBuilder.java
@@ -2,8 +2,10 @@ package histograma;
 
 public class HistogramBuilder <Type> {
     
-    public Histogram<Type> build(Type [] types){
-        return null;
+    public Histogram<Type> build(Type[] types){
+        Histogram<Type> histogram = new Histogram();
+        for (Type type : types)
+            histogram.put(type, histogram.get(type));
+        return histogram;
     }
-    
 }
